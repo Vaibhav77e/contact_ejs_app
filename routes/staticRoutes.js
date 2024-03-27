364d94fd4b9be887ef2f294506b808dca2be79b0
Vaibhav77e <111438234+Vaibhav77e@users.noreply.github.com>
2024-03-27 11:02:34 +0530

    add sperate route for edit page

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Contacts = require('../models/ContactsModel/contacts_model');
 
 const {viewAllMyContacts} = require('../controllers/ContactController/view_contact_controller');
 
@@ -18,6 +19,49 @@ router.post('/contacts/add_user',(req,res)=>{
     res.render("add_user",{title:"Add Users",body:"Add Users"});
 });
 
+// edit route
+router.get('/edit/:id',(req,res)=>{
+    let id = req.params.id;
+    Contacts.findById(id).then(contacts=>{
+        if(contacts===null){
+            res.redirect('/');
+        }else{
+            res.render('edit_user',{
+                contacts:contacts
+            });
+        }
+    }).catch(err=>{
+            res.redirect('/');
+    });
+});
+
+// update route
+
+router.post('/update/:id',(req,res)=>{
+    let id = req.params.id;
+
+    Contacts.findByIdAndUpdate(id,{
+        name:req.body.name,
+        phone:req.body.phone
+    }).then(result=>{
+        res.redirect('/');
+    }).catch(err=>{
+        console.log(`Error while updating the record : ${err.message}`);
+    });
+
+});
+
+// delete route
+
+router.get('/delete/:id',(req,res)=>{
+    let id = req.params.id;
+
+    Contacts.findByIdAndDelete(id).then(_ =>{
+        res.redirect('/');
+    }).catch(err=>{
+        console.log(`Error while deleting the users ${err.message}`);
+    });
+});
 
 
 
