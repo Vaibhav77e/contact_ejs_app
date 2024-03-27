c86d00959e9cf639936298405175cad13885a77e
Vaibhav77e <111438234+Vaibhav77e@users.noreply.github.com>
2024-03-27 12:22:20 +0530

    added login screen ui

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Contacts = require('../models/ContactsModel/contacts_model');
 
+const User = require('../models/UserModel/user_model');
+const bcrypt = require('bcryptjs');
+
 const {viewAllMyContacts} = require('../controllers/ContactController/view_contact_controller');
+const isUserAuthenticated = require('../middlewares/isUserAuthenticated');
 
 // router.get('/',(req,res)=>{
 //     res.render("index",{title:"Nation pride",body:"India the great"});
@@ -11,6 +15,55 @@ const {viewAllMyContacts} = require('../controllers/ContactController/view_conta
 // added to check whether the data will load or not working fine now
 // router.get('/view_contacts', viewAllMyContacts);
 
+
+/// <------------- Auth Routes ---------------------->
+
+
+router.get('/test',(req, res)=>{
+    res.render('test');
+})
+
+router.post('/auth/createAccount',async(req, res)=>{
+    try{
+        const {name,age,email,phone,password,address} = req.body;
+
+        let user = await User.find({email:email});
+
+        if(user.length>0){
+            return res.status(400).json({
+                message:"User already exists.Please login different account or create one"
+            });
+        }
+
+        if(password.length<=0){
+            return res.status(404).json({
+                "message":"Please provide password"
+            })
+        }
+
+        const hashedPassword =await bcrypt.hash(password,10);
+
+        user = await User.create({
+        name:name,
+        age:age,
+        email:email,
+        phone:phone,
+        password:hashedPassword,
+        address:address});
+
+        if(user===null){
+            console.log("Couldn't create account");
+            return;
+        }
+
+        res.render('/');
+    }catch(err){
+        console.log(`Error whiling creating account: ${err.message}`)
+    }
+})
+
+
+/// <------- Contacts Routes --------------------> //////
 // get the all contacts
 router.get('/',viewAllMyContacts);
 
